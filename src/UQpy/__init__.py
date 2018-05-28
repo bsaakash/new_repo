9908b81d3aa41fab16891a24301d92dc1cc7d324
bsaakash <11618528+bsaakash@users.noreply.github.com>
2018-05-27 23:46:53 -0400

    Update __init__.py

--- src/UQpy/__init__.py
+++ src/UQpy/__init__.py
@@ -1,12 +1,12 @@
-#import UQpyModules
-#import ReadInputFile
-#import Reliability
-#import SampleMethods
-#import UQpy_cmd
+import src/UQpy/UQpyModules
+import src/UQpy/ReadInputFile
+import src/UQpy/Reliability
+import src/UQpy/SampleMethods
+import src/UQpy/UQpy_cmd
 
 
-from src/UQpy/UQpyModules import *
-from src/UQpy/ReadInputFile import *
-from src/UQpy/Reliability import *
-from src/UQpy/SampleMethods import *
-from src/UQpy/UQpy_cmd import *
+#from src/UQpy/UQpyModules import *
+#from src/UQpy/ReadInputFile import *
+#from src/UQpy/Reliability import *
+#from src/UQpy/SampleMethods import *
+#from src/UQpy/UQpy_cmd import *
